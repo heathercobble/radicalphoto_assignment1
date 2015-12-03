5669647c625342ad7812b1b29f75dde53b08bc38
heathercobble <hcobble@hotmail.com>
2015-12-03 08:49:17 -0500

    www105_assignment1
    Radical Photo - files uploaded

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+/*var paragraph = document.getElementById("p-change");
+
+ function changeText(){
+            paragraph.innerHTML = "Capturing Nature";
+        }
+setTimeout(changeText, 2500); */
+
+
+
+
+/* var bluedressthumb = document.getElementById("bluedressthumb");
+var boxerthumb = document.getElementById("boxerthumb");
+
+var bigimage = document.getElementById("big_image");
+
+bluedressthumb.onmouseover = function(){
+    bigimage.src="images/blue_dress.jpg";
+}
+
+
+
+boxerthumb.onmouseover = function () {
+    bigimage.src="images/boxer.jpg";
+}
+*/
